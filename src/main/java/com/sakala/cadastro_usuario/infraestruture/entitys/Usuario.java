c231131764d2e6b3553e29f420a07e89ad7354d1
sakalasimao <sakalalusala@gmail.com>
2026-05-31 19:01:04 +0100

    Adicionando a classe usuario entity

---
+++ src/main/java/com/sakala/cadastro_usuario/infraestruture/entitys/Usuario.java
@@ -0,0 +1,26 @@
+package com.sakala.cadastro_usuario.infraestruture.entitys;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@Table(name = "usuario")
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column
+
+}
