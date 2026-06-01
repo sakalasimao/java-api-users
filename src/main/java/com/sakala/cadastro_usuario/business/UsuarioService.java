044014d4fbbf338bfadcd36bf986c5457543b282
sakalasimao <sakalalusala@gmail.com>
2026-06-01 11:01:47 +0100

    Terminando o service

--- src/main/java/com/sakala/cadastro_usuario/business/UsuarioService.java
+++ src/main/java/com/sakala/cadastro_usuario/business/UsuarioService.java
@@ -35,16 +35,23 @@ public class UsuarioService {
     }
 
 
-    public void atualizarUsuarioPorEmail(String email, Usuario usuario){
+    public void atualizarUsuarioPorId(Integer id, Usuario usuario){
 
-        Usuario usuarioEntity = buscarUsuarioPorEmail(email);
+        Usuario usuarioEntity = repository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuario não encontrando")
+        );
         Usuario usuarioAtualizado = Usuario.builder()
 
                 .email(usuario.getEmail() != null ?
                         usuario.getEmail() : usuarioEntity.getEmail())
 
+                .nome(usuario.getNome() != null ?
+                        usuario.getNome() : usuarioEntity.getNome())
 
+                .id(usuarioEntity.getId())
                 .build();
+
+        repository.saveAndFlush(usuarioAtualizado);
 
 
     }
