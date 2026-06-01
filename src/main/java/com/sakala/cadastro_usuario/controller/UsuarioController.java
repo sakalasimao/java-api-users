1956f2ca8d31b602a4573ca48ecba0a4103a97d1
sakalasimao <sakalalusala@gmail.com>
2026-06-01 11:04:36 +0100

    Adicionando a classe usuario controller

---
+++ src/main/java/com/sakala/cadastro_usuario/controller/UsuarioController.java
@@ -0,0 +1,19 @@
+package com.sakala.cadastro_usuario.controller;
+
+
+import com.sakala.cadastro_usuario.business.UsuarioService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    
+}
