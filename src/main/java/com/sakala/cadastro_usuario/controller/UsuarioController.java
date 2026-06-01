d8d7e5beef1ac33d2b820aea56c3d49d00985e39
sakalasimao <sakalalusala@gmail.com>
2026-06-01 11:15:30 +0100

    Terminando metodos salvarUsuario e buscarUsuario

--- src/main/java/com/sakala/cadastro_usuario/controller/UsuarioController.java
+++ src/main/java/com/sakala/cadastro_usuario/controller/UsuarioController.java
@@ -2,8 +2,9 @@ package com.sakala.cadastro_usuario.controller;
 
 
 import com.sakala.cadastro_usuario.business.UsuarioService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sakala.cadastro_usuario.infraestruture.entitys.Usuario;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuario")
@@ -14,6 +15,20 @@ public class UsuarioController {
     public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
     }
+
+    @PostMapping
+    public ResponseEntity<Void> salvarUsuario(@RequestBody Usuario usuario){
+        usuarioService.salvarUsuario(usuario);
+        return  ResponseEntity.ok().build();
+
+    }
+
+
+    @GetMapping
+    public ResponseEntity<Usuario> buscarUsuarioPorEmail(@RequestParam String email){
+        return  ResponseEntity.ok(usuarioService.buscarUsuarioPorEmail(email));
+    }
+
 
     
 }
